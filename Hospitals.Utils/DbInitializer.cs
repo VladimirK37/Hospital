73615458c299ef430f0394461a9e0e6019848626
VladimirK37 <61294220+VladimirK37@users.noreply.github.com>
2022-11-08 21:49:12 +0300

    Добавьте файлы проекта.

---
+++ Hospitals.Utils/DbInitializer.cs
@@ -0,0 +1,63 @@
+﻿using Hospital.Models;
+using Hospital.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospitals.Utilites
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private UserManager<IdentityUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private ApplicationDbContext _context;
+
+        public DbInitializer(UserManager<IdentityUser> userManager, 
+            RoleManager<IdentityRole> roleManager, 
+            ApplicationDbContext context)
+        {
+            _userManager=userManager;
+            _roleManager=roleManager;
+            _context=context;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                if (_context.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _context.Database.Migrate();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            if (!_roleManager.RoleExistsAsync(WebSiteRoles.WebSite_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.WebSite_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.WebSite_Patient)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.WebSite_Doctor)).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "Vova",
+                    Email="vova@xxy.com"
+
+                },"Vova@123").GetAwaiter().GetResult();
+                var Appuser = _context.ApplicationUsers.FirstOrDefault(x => x.Email == "vova@xxy.com");
+                if (Appuser != null)
+                {
+                    _userManager.AddToRoleAsync(Appuser, WebSiteRoles.WebSite_Admin).GetAwaiter().GetResult();
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ Hospitals.Utils/IDbInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospitals.Utilites
+{
+    public interface IDbInitializer
+    {
+        void Initialize();
+    }
+}
